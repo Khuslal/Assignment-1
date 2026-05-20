a04d9f065553cc57d2275d3503556b8547462c84
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-20 23:45:58 +0545

    2nd commit

--- corejava/src/javacollection/JavaCollectionDocs.java
+++ corejava/src/javacollection/JavaCollectionDocs.java
@@ -8,13 +8,13 @@ public class JavaCollectionDocs {
 	 * 		a. ArrayList : Slow Processing, better to use for data storage
 	 * 		b. Linked List : Fast Processing, better to use for data processing
 	 * 
-	 *  2. Set : collection of unique data
-	 *  	a. HashSet : unique only
-	 *  	b. TreeSet : unique + sorted
+	 *  2. Set : collection of unique data/ not repeating same value of data
+	 *  	a. HashSet : unique only | e.g. 23,35,13,35 will store only one 35 and don't follow sorted order|
+	 *  	b. TreeSet : unique + sorted (e.g. if user add 23,35,13,35 he will have 13,23,35 ascending order)
 	 *  	c. LinkedHashSet : unique + insertion in order
 	 *  
 	 *  3. Map : data manage in key, value pair
-	 *  	a. HashMap :  <k, v> pair
+	 *  	a. HashMap :  <k, v> pair | k = key, v = value |
 	 *  	b. TreeMap : <k, v> + sorted 
 	 *  	c. LinkedHashMap :  <k, v> + insertion in order
 	 */
