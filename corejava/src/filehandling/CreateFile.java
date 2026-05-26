cb90169085b3992f74a4962a2ac4d5b9846e087f
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-26 22:17:19 +0545

    File Handling -Creating File and Storing Data

---
+++ corejava/src/filehandling/CreateFile.java
@@ -0,0 +1,23 @@
+package filehandling;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CreateFile {
+	/*
+	 * 1. File InputStream : readfile 2. File OutputStream : write file
+	 */
+
+	public static void main(String[] args) throws IOException {
+		// Creating file
+		FileOutputStream file = new FileOutputStream("hello.txt");
+		// Trying to create txt file in the specified location :
+		FileOutputStream file1 = new FileOutputStream("C:\\Users\\Khuslal\\OneDrive\\Documents\\CmdNotes.txt");
+		// Writing / Inserting data
+		file.write("Hello Java".getBytes());
+		// Closing file
+		file.close();
+
+		System.out.println("File Created");
+	}
+}
---
+++ corejava/src/filehandling/ReadFile.java
@@ -0,0 +1,21 @@
+package filehandling;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadFile {
+	public static void main(String[] args) throws IOException {
+		FileInputStream file = new FileInputStream("hello.txt");
+		// Trying to access txt file from the specified location : 
+		FileInputStream file1 = new FileInputStream("C:\\Users\\Khuslal\\OneDrive\\Documents\\CmdNotes.txt");
+		int data;
+		while((data = file.read())!= -1) {
+			System.out.print((char)data);
+		}
+		int data1;
+		while((data1 = file1.read())!= -1) {
+			System.out.print((char)data1);
+		}
+	}
+}
