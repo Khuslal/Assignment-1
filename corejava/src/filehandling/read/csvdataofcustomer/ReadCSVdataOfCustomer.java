a632f3bb9e55d690aa2ea83b7ce7bc1785cb3b61
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-29 13:35:15 +0545

    data successfully stored inside database : customerdb table : customer

--- corejava/src/filehandling/read/csvdataofcustomer/ReadCSVdataOfCustomer.java
+++ corejava/src/filehandling/read/csvdataofcustomer/ReadCSVdataOfCustomer.java
@@ -6,22 +6,46 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 
 public class ReadCSVdataOfCustomer {
 	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, SQLException {
-		
+
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb","root","password");
+
+		String url = "jdbc:mysql://localhost:3306/customerdb";
+		String user = "root";
+		String password = "password";
+		String upload = "insert into customers (id, name, address, phone, product) values (?, ?, ?, ?, ?)";
+
 		try {
-			FileReader fr = new FileReader("Customer.csv");
-			BufferedReader br = new BufferedReader(fr);
+			BufferedReader br = new BufferedReader(new FileReader("Customer.csv"));
+			Connection conn = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = conn.prepareStatement(upload);
+
 			String line;
+			boolean isHeader = true; // flag to handle csv header row
 
 			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+				// skip first row i.e. id:, name:, address:, phone:, product:
+				if (isHeader) {
+					isHeader = false;
+					continue;
+				}
+				String[] customers = line.split(",");
+
+				// Map the array elements to your sql query placeholders (?)
+				pstmt.setString(1, customers[0].trim()); // id
+				pstmt.setString(2, customers[1].trim()); // name
+				pstmt.setString(3, customers[2].trim()); // address
+				pstmt.setString(4, customers[3].trim()); // phone
+				pstmt.setString(5, customers[4].trim()); // product
+				pstmt.executeUpdate();
 			}
+			
+			System.out.println("Data Stored Successfully");
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
